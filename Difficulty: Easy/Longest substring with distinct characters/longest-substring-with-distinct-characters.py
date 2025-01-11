b89c092538d65abd97114886b0674bf213356160
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-01-11 17:38:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest substring with distinct characters/longest-substring-with-distinct-characters.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    def longestUniqueSubstr(self, s):
+        # code here
+        d=dict()
+        if not s:
+            return 0
+        if not s.strip() or len(s)==1:
+            return 1
+        maxi=0
+        start=0
+        for i in range(len(s)):
+            if s[i] in d and d[s[i]]>=start:
+                start=d[s[i]]+1
+            d[s[i]]=i
+            maxi=max(maxi,i-start+1)
+        return maxi
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        s = input()
+
+        solObj = Solution()
+
+        ans = solObj.longestUniqueSubstr(s)
+
+        print(ans)
+
+        print("~")
+# } Driver Code Ends

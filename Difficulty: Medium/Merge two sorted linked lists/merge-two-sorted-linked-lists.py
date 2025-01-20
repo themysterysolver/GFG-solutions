ff422c0326fd5c79e863a62944da4a9320953a38
R.Prabhakara Arjun <arjunprabhakar1910@gmail.com>
2025-01-20 17:01:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.py
@@ -0,0 +1,78 @@
+#User function Template for python3
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+'''
+class Solution:
+    def sortedMerge(self,head1, head2):
+        # code here
+        if head1 is None:
+            return head2
+        if head2 is None:
+            return head1
+    
+        # Recursive merging based on smaller value
+        if head1.data <= head2.data:
+            head1.next = self.sortedMerge(head1.next, head2)
+            return head1
+        else:
+            head2.next = self.sortedMerge(head1, head2.next)
+            return head2
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(head):
+    temp = head
+    while temp:
+        print(temp.data, end=' ')
+        temp = temp.next
+    print()
+    print("~")
+
+
+def insert_sorted(head, data):
+    new_node = Node(data)
+    if not head or head.data >= data:
+        new_node.next = head
+        return new_node
+
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+
+    new_node.next = current.next
+    current.next = new_node
+
+    return head
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        input1 = list(map(int, input().split()))
+        input2 = list(map(int, input().split()))
+
+        head1 = None
+        for item in input1:
+            head1 = insert_sorted(head1, item)
+
+        head2 = None
+        for item in input2:
+            head2 = insert_sorted(head2, item)
+
+        obj = Solution()
+        merged_head = obj.sortedMerge(head1, head2)
+        print_list(merged_head)
+
+# } Driver Code Ends
